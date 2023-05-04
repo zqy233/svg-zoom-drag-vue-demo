7c028a1a1783d7f52883c7741d075c778663264a
zqylzcwcxy <347891134@qq.com>
2023-05-04 16:06:24 +0800

    :sparkles:feat: 完善指令类型

--- src/directive/svg.ts
+++ src/directive/svg.ts
@@ -1,15 +1,18 @@
+import { App } from "vue";
+
 /** 控制svg图片放大缩小，原理在于设置svg的viewbox属性，viewbox的第三个参数控制左右大小，第四个参数设置上下大小 */
-export function svgWheel(app: any) {
+export function svgWheel(app: App) {
   app.directive("svgWheel", (el: HTMLElement) => {
     if (el) {
-      el.onwheel = (e: any) => {
+      el.onwheel = (e: WheelEvent) => {
         // 因为使用v-html渲染svg，所以绑定命令的dom的子元素才是svg元素
         const sgvDom = el.firstChild as SVGAElement;
         // 获取svg的viewbox属性
         const viewBox = sgvDom.getAttribute("viewBox") as string;
         const arrPoint = viewBox.split(/\s+/);
-        // event.wheelDelta正数表示向上滚动，负数表示向下滚动
-        if (e.wheelDelta > 0) {
+        // event.wheelDelta废弃了，使用event.deltaY代替
+        // 正数表示向上滚动，负数表示向下滚动
+        if (e.deltaY > 0) {
           arrPoint[2] = parseInt(arrPoint[2]) * 0.9 + "";
           arrPoint[3] = parseInt(arrPoint[3]) * 0.9 + "";
         } else {
@@ -23,7 +26,7 @@ export function svgWheel(app: any) {
 }
 
 /** 控制svg图片拖动，原理在于设置svg的viewbox属性，viewbox的第一个参数控制左右位置，第二个参数设置上下位置  */
-export function svgDrag(app: any) {
+export function svgDrag(app: App) {
   app.directive("svgDrag", (el: HTMLElement) => {
     let clientX = 0; // 上一次点的x轴的位置
     let clientY = 0; // 上一次点的y轴的位置
